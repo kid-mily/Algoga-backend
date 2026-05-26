7fea2b4857bf45efd13b70b178016220876d4530
JindoKim <kjd020322@gmail.com>
2026-05-26 21:03:38 +0900

    [FIX] Notice 도메인 매니저 pk 값 전달 추가

--- src/main/java/com/kidmily/algoga_server/notice/application/command/CreateNoticeCommand.java
+++ src/main/java/com/kidmily/algoga_server/notice/application/command/CreateNoticeCommand.java
@@ -5,6 +5,6 @@ import com.kidmily.algoga_server.notice.presentation.NoticeTagType;
 public record CreateNoticeCommand(
         String title,
         String content,
-        NoticeTagType noticeTagType
-) {
-}
+        NoticeTagType noticeTagType,
+        Long managerId // 🌟 매니저 ID 필드 추가
+) {}
