1190fc7582312d2de1bfc337e7138a5e03377306
baek1885-dev <beak1885@g.eulji.ac.kr>
2026-06-10 00:19:20 +0900

    신고 도메인 외부 분리, 게시글 댓글 소프트 딜리트 연동 수정

--- src/main/java/com/kidmily/algoga_server/community/application/usecase/ReportCommandUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.kidmily.algoga_server.community.application.usecase;
-
-import com.kidmily.algoga_server.community.application.command.CreateReportCommand;
-
-public interface ReportCommandUseCase {
-    Long handle(CreateReportCommand command);
-}
