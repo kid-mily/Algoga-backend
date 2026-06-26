9ea5c3b0e14d8484b3ac2af5a3f0d30d73894290
JindoKim <kjd020322@gmail.com>
2026-06-26 11:14:54 +0900

    [FIX] notice 캐싱 충돌 해결

--- src/main/java/com/kidmily/algoga_server/notice/presentation/api/response/NoticeListResponse.java
+++ src/main/java/com/kidmily/algoga_server/notice/presentation/api/response/NoticeListResponse.java
@@ -1,8 +1,10 @@
 package com.kidmily.algoga_server.notice.presentation.api.response;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo; // 🌟 추가
 import com.kidmily.algoga_server.notice.presentation.NoticeTagType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class") // 🌟 추가
 @Schema(description = "공지사항 전체 목록 응답 DTO (본문 제외)")
 public record NoticeListResponse(
         @Schema(description = "공지사항 ID", example = "1")
