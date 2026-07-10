6fb58e8a6638130601498f01a5f994791317d8c6
baek1885-dev <beak1885@g.eulji.ac.kr>
2026-07-10 18:34:29 +0900

    태그명 수정

--- src/main/java/com/kidmily/algoga_server/community/domain/model/PostTagType.java
+++ src/main/java/com/kidmily/algoga_server/community/domain/model/PostTagType.java
@@ -13,7 +13,7 @@ public enum PostTagType {
     COMPANION("동행 구해요"),
     COUNTRY("나라"),
     LECTURE("강의후기"),
-    FREE("자유(커스텀태그)");
+    FREE("자유");
 
     // 한글 명칭을 저장할 변수 (final로 안전하게 보호)
     private final String description;
